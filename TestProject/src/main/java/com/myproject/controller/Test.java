cf271644a32694ed07eca2f57630b79051193069
santosh <santosh@santosh-Inspiron-14-3467>
2022-03-24 13:00:34 +0530

    google and doit

--- TestProject/src/main/java/com/myproject/controller/Test.java
+++ TestProject/src/main/java/com/myproject/controller/Test.java
@@ -1,13 +1,33 @@
 package com.myproject.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 public class Test {
 	@GetMapping("/test1")
+	@ResponseBody
 	public String test1() {
 		return "this is test functioon for online testing";
 	}
+	
+	@GetMapping("/goto/{name}")
+	public String test1(@PathVariable("name") String name) {
+		
+		if(name.equalsIgnoreCase("google")) {
+			 
+		       return "redirect:https://www.google.com";
+		}
+		if(name.equalsIgnoreCase("doit")) {
+			 
+		       return "redirect:https://doitc.rajasthan.gov.in/";
+		}
+		return null;
+	}
+	
+	
 	
 }
